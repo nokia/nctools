56b5d74f3bdc03834d81224cb7ccfe54d24dee55
U-NSN-INTRA\swisotzk <swisotzk@DESTGN0H331972.nsn-intra.net>
2017-09-04 16:00:55 +0200

    version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# nctools
-Python tools for NETCONF
+# nctools - Python tools for NETCONF
+"nctools" is a collection of NETCONF tools in Python using the paramiko SSHv2 library.
+
+## ncproxy - NETCONF Proxy
+The tool "ncproxy" is a transparent NETCONF proxy. It is deployed between the NETCONF
+server and NETCONF client to provide logging capabilities. From the NETCONF server
+point of view ncproxy acts as client and from the NETCONF client point of view it
+acts as server. All hello messages, RPC requests, RPC responses and notification
+messages are subject of logging.
+
+The ncproxy tool is helpful for network integrators, who want to troubleshoot NETCONF
+without having logging capabilities for neither the server nor the client. Capturing
+the SSHv2 traffic using tools like tcpdump, snoop or wireshark does typically not help,
+as there is no easy way to break SSHv2 privacy.
+ 
---
+++ ncproxy.py
@@ -0,0 +1,412 @@
+#!/usr/bin/python
+##############################################################################
+#                                                                            #
+#  ncproxy.py                                                                #
+#                                                                            #
+#  History Change Log:                                                       #
+#                                                                            #
+#    1.0  [SW]  2017/09/04    first version                                  #
+#                                                                            #
+#  Objective:                                                                #
+#    ncproxy is a transparent logging proxy for NETONF over SSH              #
+#                                                                            #
+#  License:                                                                  #
+#    Licensed under the BSD license                                          #
+#    See LICENSE.md delivered with this project for more information.        #
+#                                                                            #
+#  Author:                                                                   #
+#    Sven Wisotzky                                                           #
+#    mail:  sven.wisotzky(at)nokia.com                                       #
+#                                                                            #
+#                                           (c) 2017 by Sven Wisotzky, Nokia #
+##############################################################################
+
+"""
+NETCONF proxy in Python Version 1.0
+Copyright (C) 2015-2017 Nokia. All Rights Reserved.
+"""
+
+import binascii
+import logging
+import os
+import paramiko
+import socket
+import sys
+import threading
+import time
+import traceback
+import argparse
+
+if sys.version_info > (3,):
+    from urllib.parse import urlparse
+else:
+    from urlparse import urlparse
+
+__title__ = "ncproxy"
+__version__ = "1.0"
+__status__ = "released"
+__author__ = "Sven Wisotzky"
+__date__ = "2017 September 4th"
+
+
+class ncHandler(paramiko.SubsystemHandler):
+
+    def __init__(self, channel, name, server, username, password):
+        paramiko.SubsystemHandler.__init__(self, channel, name, server)
+        self.__username = username
+        self.__password = password
+
+    def start_subsystem(self, name, transport, channel):
+        try:
+            log.info('Establish NETCONF over SSH connection to %s:%d user(%s)', url.hostname, url.port or 830, self.__username)
+            srv_tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            srv_tcpsock.connect((url.hostname, url.port or 830))
+            srv_transport = paramiko.Transport(srv_tcpsock)
+            srv_transport.connect(username=self.__username, password=self.__password)
+            srv_channel = srv_transport.open_session()
+            srv_channel.invoke_subsystem('netconf')
+
+        except Exception as e:
+            # --- close channel/transport to NETCONF client ------------------
+            log.warning('NETCONF over SSH to %s failed: %s', url.hostname, str(e))
+            channel.close()
+            transport.close()
+            return
+
+        log.info('NETCONF messaging capture')
+
+        nccbuf = ""
+        srvbuf = ""
+
+        while transport.is_active():
+            # --- receive bytes from server, append to srvbuf ----------------
+
+            while srv_channel.recv_ready():
+                srvbuf += srv_channel.recv(65535)
+
+            # --- extract srvmsgs[] from srvbuf ------------------------------
+
+            srvmsgs = []
+            if len(srvbuf) > 4:
+                if srvbuf[0:2] != "\n#":
+                    base10 = True   # base:1.0 framing (EOM)
+                    srvmsgs = srvbuf.split("]]>]]>")
+                    srvbuf = srvmsgs.pop()
+                else:
+                    base10 = False  # --- base:1.1 framing (chunks) ----------
+
+                    tmp = ""
+                    pos = 0
+
+                    while pos < len(srvbuf) and len(srvbuf) > 4:
+                        if srvbuf[pos:pos + 4] == "\n##\n":
+                            srvmsgs.append(tmp)
+                            tmp = ""
+                            srvbuf = srvbuf[pos + 4:]
+                            pos = 0
+                        elif srvbuf[pos:pos + 2] == "\n#":
+                            idx = srvbuf.find("\n", pos + 2)
+                            if idx != -1:
+                                bytes = int(srvbuf[pos + 2:idx])
+                                tmp += srvbuf[idx + 1:idx + 1 + bytes]
+                                pos = idx + 1 + bytes
+                            else:
+                                # --- need to wait for more bytes to come ----
+                                break
+                        else:
+                            log.error('SERVER FRAMING ERROR')
+                            srvbuf = ""
+                            break
+
+            # --- forward and print srvmsgs[] --------------------------------
+
+            for msg in srvmsgs:
+                # --- send/print what we've got ------------------------------
+                if not base10:
+                    buf = "\n#%d\n" % len(msg)
+                    channel.send(buf)
+                    serverlog.write(buf)
+
+                pos = 0
+                while pos < len(msg):
+                    if pos + 16384 < len(msg):
+                        buf = msg[pos:pos + 16384]
+                        pos += 16384
+                    else:
+                        buf = msg[pos:]
+                        pos = len(msg)
+                    channel.send(buf)
+                    serverlog.write(buf)
+
+                if base10:
+                    buf = "]]>]]>"
+                else:
+                    buf = "\n##\n"
+                channel.send(buf)
+                serverlog.write(buf)
+                serverlog.flush()
+
+            # --- receive bytes from client, append to nccbuf ----------------
+
+            while channel.recv_ready():
+                nccbuf += channel.recv(65535)
+
+            # --- extract nccmsgs[] from nccbuf ------------------------------
+
+            nccmsgs = []
+            if len(nccbuf) > 4:
+                if nccbuf[0:2] != "\n#":
+                    base10 = True   # base:1.0 framing (EOM)
+                    nccmsgs = nccbuf.split("]]>]]>")
+                    nccbuf = nccmsgs.pop()
+                else:
+                    base10 = False  # --- base:1.1 framing (chunks) ----------
+
+                    tmp = ""
+                    pos = 0
+
+                    while pos < len(nccbuf) and len(nccbuf) > 4:
+                        if nccbuf[pos:pos + 4] == "\n##\n":
+                            nccmsgs.append(tmp)
+                            tmp = ""
+                            nccbuf = nccbuf[pos + 4:]
+                            pos = 0
+                        elif nccbuf[pos:pos + 2] == "\n#":
+                            idx = nccbuf.find("\n", pos + 2)
+                            if idx != -1:
+                                bytes = int(nccbuf[pos + 2:idx])
+                                tmp += nccbuf[idx + 1:idx + 1 + bytes]
+                                pos = idx + 1 + bytes
+                            else:
+                                # --- need to wait for more bytes to come ----
+                                break
+                        else:
+                            log.error('CLIENT FRAMING ERROR')
+                            nccbuf = ""
+                            break
+
+            # --- forward and print nccmsgs[] --------------------------------
+
+            for msg in nccmsgs:
+                if not base10:
+                    buf = "\n#%d\n" % len(msg)
+                    srv_channel.send(buf)
+                    clientlog.write(buf)
+
+                pos = 0
+                while pos < len(msg):
+                    if pos + 16384 < len(msg):
+                        buf = msg[pos:pos + 16384]
+                        pos += 16384
+                    else:
+                        buf = msg[pos:]
+                        pos = len(msg)
+                    srv_channel.send(buf)
+                    clientlog.write(buf)
+
+                if base10:
+                    buf = "]]>]]>"
+                else:
+                    buf = "\n##\n"
+                srv_channel.send(buf)
+                clientlog.write(buf)
+                clientlog.flush()
+
+            if srv_channel.exit_status_ready():
+                break
+            if channel.exit_status_ready():
+                break
+            time.sleep(0.01)
+
+        else:
+            serverlog.flush()
+            clientlog.flush()
+            log.info('NETCONF communication finished')
+
+        if srv_channel.exit_status_ready():
+            log.warning("Connection closed by peer; server down")
+        if channel.exit_status_ready():
+            log.warning("Connection closed by peer; client down")
+
+        # close channel/transport to NETCONF server
+        srv_channel.close()
+        srv_transport.close()
+        srv_tcpsock.close()
+
+        # close channel/transport to NETCONF client
+        channel.close()
+        transport.close()
+
+
+class ssh_server(paramiko.ServerInterface):
+
+    def __init__(self):
+        self.event = threading.Event()
+
+    def check_channel_request(self, kind, chanid):
+        log.debug("ssh_server.check_channel_request(kind=%s, chanid=%s)",  kind, chanid)
+        if kind == 'session':
+            return paramiko.OPEN_SUCCEEDED
+        return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
+
+    def check_auth_password(self, username, password):
+        log.debug("ssh_server.check_auth_password(username=%s, password=%s)", username, password)
+        self.username = username
+        self.password = password
+        return paramiko.AUTH_SUCCESSFUL
+
+    def check_auth_publickey(self, username, key):
+        log.debug("ssh_server.check_auth_publickey()")
+        log.critical('Public key authentication is NOT supported')
+        return paramiko.AUTH_FAILED
+
+    def get_allowed_auths(self, username):
+        log.debug("ssh_server.get_allowed_auths(username=%s)", username)
+        return 'password'
+
+    def check_channel_shell_request(self, channel):
+        log.debug("ssh_server.check_channel_shell_request()")
+        log.critical('SHELL request is NOT supported')
+        self.event.set()
+        return False
+
+    def check_channel_exec_request(self, channel, command):
+        log.debug("ssh_server.check_channel_exec_request()")
+        log.critical('EXEC request is NOT supported')
+        return False
+
+    def check_channel_pty_request(self, channel, term, width, height, pixelwidth, pixelheight, modes):
+        log.debug("ssh_server.check_channel_pty_request(term=%s, width=%d, height=%d)", term, width, height)
+        log.critical('PTY request is NOT supported')
+        return False
+
+    def check_channel_subsystem_request(self, channel, name):
+        log.debug("ssh_server.check_channel_subsystem_request(name=%s)", name)
+        if name == 'netconf':
+            handler = ncHandler(
+                channel, name, self, self.username, self.password)
+            handler.start()
+            return True
+        log.critical('Subsystem %s is NOT supported', name)
+        return False
+
+
+if __name__ == '__main__':
+    prog = os.path.splitext(os.path.basename(sys.argv[0]))[0]
+    dlog = prog + '.log'
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--version', action='version', version=prog + ' ' + __version__)
+
+    group = parser.add_argument_group()
+    group.add_argument('-v', '--verbose', action='count', help='enable logging')
+    group.add_argument('-d', '--debug', action='count', help='enable ssh-lib logging')
+    group.add_argument('--logfile', metavar='<filename>', default=dlog, type=argparse.FileType('wb', 0), help='Specify the logfile (default: %s)' % dlog)
+    group.add_argument('--serverlog', metavar='<filename>', default='-', type=argparse.FileType('wb', 0), help='Specify the server log (default: <stdout>)')
+    group.add_argument('--clientlog', metavar='<filename>', default='-', type=argparse.FileType('wb', 0), help='Specify the client log (default: <stdout>)')
+
+    group = parser.add_argument_group()
+    group.add_argument('--port', metavar='tcpport', type=int, default=830, help='TCP-port ncproxy is listening')
+    group.add_argument('server', metavar='netconf://<hostname>[:port]', default="netconf://127.0.0.1:830", help='Netconf over SSH server (default: <netconf://127.0.0.1:830)>)')
+
+    options = parser.parse_args()
+
+    # --- setup module logging -----------------------------------------------
+    loghandler = logging.StreamHandler(options.logfile)
+    timeformat = '%y/%m/%d %H:%M:%S'
+    logformat = '%(asctime)s,%(msecs)-3d %(levelname)-8s %(message)s'
+    loghandler.setFormatter(logging.Formatter(logformat, timeformat))
+
+    log = logging.getLogger('paramiko')
+    if options.debug is None:
+        log.setLevel(logging.NOTSET)
+        log.addHandler(logging.NullHandler())
+    elif options.debug == 1:
+        log.setLevel(logging.CRITICAL)
+        log.addHandler(loghandler)
+    elif options.debug == 2:
+        log.setLevel(logging.ERROR)
+        log.addHandler(loghandler)
+    elif options.debug == 3:
+        log.setLevel(logging.WARNING)
+        log.addHandler(loghandler)
+    elif options.debug == 4:
+        log.setLevel(logging.INFO)
+        log.addHandler(loghandler)
+    else:
+        log.setLevel(logging.DEBUG)
+        log.addHandler(loghandler)
+
+    log = logging.getLogger('ncproxy')
+    if options.verbose is None:
+        log.setLevel(logging.NOTSET)
+        log.addHandler(logging.NullHandler())
+    elif options.verbose == 1:
+        log.setLevel(logging.CRITICAL)
+        log.addHandler(loghandler)
+    elif options.verbose == 2:
+        log.setLevel(logging.ERROR)
+        log.addHandler(loghandler)
+    elif options.verbose == 3:
+        log.setLevel(logging.WARNING)
+        log.addHandler(loghandler)
+    elif options.verbose == 4:
+        log.setLevel(logging.INFO)
+        log.addHandler(loghandler)
+    else:
+        log.setLevel(logging.DEBUG)
+        log.addHandler(loghandler)
+
+    # --- set server/client log ----------------------------------------------
+    serverlog = options.serverlog
+    clientlog = options.clientlog
+
+    # --- parse server URL ---------------------------------------------------
+    if options.server.find('://') == -1:
+        url = urlparse("netconf://" + options.server)
+    else:
+        url = urlparse(options.server)
+
+    if url.scheme != "netconf":
+        log.critical('Connection to NETCONF server(s) only')
+        sys.exit(1)
+
+    # --- waiting for incoming client connections ----------------------------
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.settimeout(None)
+        sock.bind(('', options.port))
+        sock.listen(100)
+        log.info('Listening for client connection ...')
+    except Exception as e:
+        log.critical('Server setup failed: %s', str(e))
+        log.debug(''.join(traceback.format_exception(*sys.exc_info())))
+        sys.exit(1)
+
+    # --- handler for incoming client connections ----------------------------
+    host_key = paramiko.RSAKey.generate(1024)
+    log.debug('Server Key: %s', binascii.hexlify(host_key.get_fingerprint()))
+
+    while True:
+        try:
+            client, addr = sock.accept()
+            log.info("Incoming client connection from %s (srcport: %d)", addr[0], addr[1])
+        except (KeyboardInterrupt, SystemExit):
+            log.info('ncproxy terminated by user')
+            sys.exit(1)
+        except Exception as e:
+            log.critical('Server listen failure: %s', str(e))
+            log.debug(''.join(traceback.format_exception(*sys.exc_info())))
+            sys.exit(1)
+
+        try:
+            t = paramiko.Transport(client)
+            t.load_server_moduli()
+            t.add_server_key(host_key)
+            t.set_subsystem_handler('netconf', ncHandler)
+            t.start_server(server=ssh_server())
+        except Exception as e:
+            log.warning('Connection failed: %s', str(e))
+
+# EOF
